1fbf505c5461284a99fe3bc84a8bd2245dd41105
crina554 <munteanu.crina25@gmail.com>
2025-05-14 23:47:42 +0300

    Add files via upload

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class frmCalculDeducereTVA
+Partial Class frmCronometru
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,225 +22,187 @@ Partial Class frmCalculDeducereTVA
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        GroupBox1 = New GroupBox()
-        Label1 = New Label()
-        txtValFTVA = New TextBox()
-        Label2 = New Label()
-        txtTVA = New TextBox()
-        btnCalcul = New Button()
-        Label3 = New Label()
-        txtValCuTVA = New TextBox()
-        GroupBox2 = New GroupBox()
-        txtDValFTVA = New TextBox()
-        Label6 = New Label()
-        btnDeducere = New Button()
-        txtDTVA = New TextBox()
-        Label5 = New Label()
-        txtDValCuTVA = New TextBox()
-        Label4 = New Label()
-        btnClear = New Button()
-        btnClose = New Button()
-        GroupBox2.SuspendLayout()
+        components = New ComponentModel.Container()
+        lblOre = New Label()
+        lblMinute = New Label()
+        lblSecunde = New Label()
+        lblZecimi = New Label()
+        txtOra = New TextBox()
+        txtZecimi = New TextBox()
+        txtSecunde = New TextBox()
+        txtMinute = New TextBox()
+        btnStart = New Button()
+        btnReset = New Button()
+        prgOre = New ProgressBar()
+        prgSecunde = New ProgressBar()
+        prgMinute = New ProgressBar()
+        btnCeas = New Button()
+        tmrCronometru = New Timer(components)
+        tmrCeas = New Timer(components)
         SuspendLayout()
         ' 
-        ' GroupBox1
+        ' lblOre
         ' 
-        GroupBox1.Location = New Point(12, 29)
-        GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(306, 177)
-        GroupBox1.TabIndex = 0
-        GroupBox1.TabStop = False
-        GroupBox1.Text = "Calcul TVA"
+        lblOre.AutoSize = True
+        lblOre.Location = New Point(27, 38)
+        lblOre.Name = "lblOre"
+        lblOre.Size = New Size(26, 15)
+        lblOre.TabIndex = 0
+        lblOre.Text = "Ore"
         ' 
-        ' Label1
+        ' lblMinute
         ' 
-        Label1.AutoSize = True
-        Label1.Location = New Point(12, 72)
-        Label1.Name = "Label1"
-        Label1.Size = New Size(91, 15)
-        Label1.TabIndex = 1
-        Label1.Text = "Valoare fara TVA"
+        lblMinute.AutoSize = True
+        lblMinute.Location = New Point(132, 38)
+        lblMinute.Name = "lblMinute"
+        lblMinute.Size = New Size(45, 15)
+        lblMinute.TabIndex = 1
+        lblMinute.Text = "Minute"
         ' 
-        ' txtValFTVA
+        ' lblSecunde
         ' 
-        txtValFTVA.Location = New Point(109, 69)
-        txtValFTVA.Name = "txtValFTVA"
-        txtValFTVA.Size = New Size(188, 23)
-        txtValFTVA.TabIndex = 2
+        lblSecunde.AutoSize = True
+        lblSecunde.Location = New Point(254, 38)
+        lblSecunde.Name = "lblSecunde"
+        lblSecunde.Size = New Size(52, 15)
+        lblSecunde.TabIndex = 2
+        lblSecunde.Text = "Secunde"
         ' 
-        ' Label2
+        ' lblZecimi
         ' 
-        Label2.AutoSize = True
-        Label2.Location = New Point(12, 115)
-        Label2.Name = "Label2"
-        Label2.Size = New Size(27, 15)
-        Label2.TabIndex = 3
-        Label2.Text = "TVA"
+        lblZecimi.AutoSize = True
+        lblZecimi.Location = New Point(359, 38)
+        lblZecimi.Name = "lblZecimi"
+        lblZecimi.Size = New Size(43, 15)
+        lblZecimi.TabIndex = 3
+        lblZecimi.Text = "Zecimi"
         ' 
-        ' txtTVA
+        ' txtOra
         ' 
-        txtTVA.Location = New Point(109, 112)
-        txtTVA.Name = "txtTVA"
-        txtTVA.Size = New Size(94, 23)
-        txtTVA.TabIndex = 4
+        txtOra.Location = New Point(27, 56)
+        txtOra.Name = "txtOra"
+        txtOra.Size = New Size(81, 23)
+        txtOra.TabIndex = 4
         ' 
-        ' btnCalcul
+        ' txtZecimi
         ' 
-        btnCalcul.Location = New Point(222, 112)
-        btnCalcul.Name = "btnCalcul"
-        btnCalcul.Size = New Size(75, 23)
-        btnCalcul.TabIndex = 5
-        btnCalcul.Text = "Calcul TVA"
-        btnCalcul.UseVisualStyleBackColor = True
+        txtZecimi.Location = New Point(359, 56)
+        txtZecimi.Name = "txtZecimi"
+        txtZecimi.Size = New Size(81, 23)
+        txtZecimi.TabIndex = 5
         ' 
-        ' Label3
+        ' txtSecunde
         ' 
-        Label3.AutoSize = True
-        Label3.Location = New Point(12, 157)
-        Label3.Name = "Label3"
-        Label3.Size = New Size(84, 15)
-        Label3.TabIndex = 6
-        Label3.Text = "Valoare cu TVA"
+        txtSecunde.Location = New Point(254, 56)
+        txtSecunde.Name = "txtSecunde"
+        txtSecunde.Size = New Size(81, 23)
+        txtSecunde.TabIndex = 6
         ' 
-        ' txtValCuTVA
+        ' txtMinute
         ' 
-        txtValCuTVA.Location = New Point(109, 154)
-        txtValCuTVA.Name = "txtValCuTVA"
-        txtValCuTVA.Size = New Size(188, 23)
-        txtValCuTVA.TabIndex = 7
+        txtMinute.Location = New Point(132, 56)
+        txtMinute.Name = "txtMinute"
+        txtMinute.Size = New Size(81, 23)
+        txtMinute.TabIndex = 7
         ' 
-        ' GroupBox2
+        ' btnStart
         ' 
-        GroupBox2.Controls.Add(txtDValFTVA)
-        GroupBox2.Controls.Add(Label6)
-        GroupBox2.Controls.Add(btnDeducere)
-        GroupBox2.Controls.Add(txtDTVA)
-        GroupBox2.Controls.Add(Label5)
-        GroupBox2.Controls.Add(txtDValCuTVA)
-        GroupBox2.Controls.Add(Label4)
-        GroupBox2.Location = New Point(12, 212)
-        GroupBox2.Name = "GroupBox2"
-        GroupBox2.Size = New Size(306, 226)
-        GroupBox2.TabIndex = 8
-        GroupBox2.TabStop = False
-        GroupBox2.Text = "Deducere TVA"
+        btnStart.Location = New Point(144, 118)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(175, 27)
+        btnStart.TabIndex = 8
+        btnStart.Text = "Pornire cronomteru"
+        btnStart.UseVisualStyleBackColor = True
         ' 
-        ' txtDValFTVA
+        ' btnReset
         ' 
-        txtDValFTVA.Location = New Point(95, 124)
-        txtDValFTVA.Name = "txtDValFTVA"
-        txtDValFTVA.Size = New Size(188, 23)
-        txtDValFTVA.TabIndex = 13
+        btnReset.Location = New Point(144, 160)
+        btnReset.Name = "btnReset"
+        btnReset.Size = New Size(175, 27)
+        btnReset.TabIndex = 9
+        btnReset.Text = "Reset"
+        btnReset.UseVisualStyleBackColor = True
         ' 
-        ' Label6
+        ' prgOre
         ' 
-        Label6.AutoSize = True
-        Label6.Location = New Point(0, 127)
-        Label6.Name = "Label6"
-        Label6.Size = New Size(91, 15)
-        Label6.TabIndex = 9
-        Label6.Text = "Valoare fara TVA"
+        prgOre.Location = New Point(61, 211)
+        prgOre.Maximum = 24
+        prgOre.Name = "prgOre"
+        prgOre.Size = New Size(100, 23)
+        prgOre.TabIndex = 11
         ' 
-        ' btnDeducere
+        ' prgSecunde
         ' 
-        btnDeducere.Location = New Point(210, 79)
-        btnDeducere.Name = "btnDeducere"
-        btnDeducere.Size = New Size(75, 23)
-        btnDeducere.TabIndex = 12
-        btnDeducere.Text = "Deducere TVA"
-        btnDeducere.UseVisualStyleBackColor = True
+        prgSecunde.Location = New Point(302, 211)
+        prgSecunde.Maximum = 60
+        prgSecunde.Name = "prgSecunde"
+        prgSecunde.Size = New Size(100, 23)
+        prgSecunde.TabIndex = 12
         ' 
-        ' txtDTVA
+        ' prgMinute
         ' 
-        txtDTVA.Location = New Point(97, 80)
-        txtDTVA.Name = "txtDTVA"
-        txtDTVA.Size = New Size(94, 23)
-        txtDTVA.TabIndex = 11
+        prgMinute.Location = New Point(185, 211)
+        prgMinute.Maximum = 60
+        prgMinute.Name = "prgMinute"
+        prgMinute.Size = New Size(100, 23)
+        prgMinute.TabIndex = 13
         ' 
-        ' Label5
+        ' btnCeas
         ' 
-        Label5.AutoSize = True
-        Label5.Location = New Point(0, 88)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(27, 15)
-        Label5.TabIndex = 10
-        Label5.Text = "TVA"
+        btnCeas.Location = New Point(163, 266)
+        btnCeas.Name = "btnCeas"
+        btnCeas.Size = New Size(143, 23)
+        btnCeas.TabIndex = 14
+        btnCeas.Text = "Afisare ceas"
+        btnCeas.UseVisualStyleBackColor = True
         ' 
-        ' txtDValCuTVA
+        ' tmrCronometru
         ' 
-        txtDValCuTVA.Location = New Point(97, 39)
-        txtDValCuTVA.Name = "txtDValCuTVA"
-        txtDValCuTVA.Size = New Size(188, 23)
-        txtDValCuTVA.TabIndex = 9
         ' 
-        ' Label4
+        ' tmrCeas
         ' 
-        Label4.AutoSize = True
-        Label4.Location = New Point(0, 42)
-        Label4.Name = "Label4"
-        Label4.Size = New Size(84, 15)
-        Label4.TabIndex = 9
-        Label4.Text = "Valoare cu TVA"
+        tmrCeas.Interval = 1000
         ' 
-        ' btnClear
-        ' 
-        btnClear.Location = New Point(12, 394)
-        btnClear.Name = "btnClear"
-        btnClear.Size = New Size(75, 23)
-        btnClear.TabIndex = 9
-        btnClear.Text = "Clear"
-        btnClear.UseVisualStyleBackColor = True
-        ' 
-        ' btnClose
-        ' 
-        btnClose.Location = New Point(220, 394)
-        btnClose.Name = "btnClose"
-        btnClose.Size = New Size(75, 23)
-        btnClose.TabIndex = 10
-        btnClose.Text = "Close"
-        btnClose.UseVisualStyleBackColor = True
-        ' 
-        ' frmCalculDeducereTVA
+        ' frmCronometru
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(309, 450)
-        Controls.Add(btnClose)
-        Controls.Add(btnClear)
-        Controls.Add(GroupBox2)
-        Controls.Add(txtValCuTVA)
-        Controls.Add(Label3)
-        Controls.Add(btnCalcul)
-        Controls.Add(txtTVA)
-        Controls.Add(Label2)
-        Controls.Add(txtValFTVA)
-        Controls.Add(Label1)
-        Controls.Add(GroupBox1)
-        Name = "frmCalculDeducereTVA"
-        Text = "Calcul & Deducere TVA"
-        GroupBox2.ResumeLayout(False)
-        GroupBox2.PerformLayout()
+        ClientSize = New Size(471, 331)
+        Controls.Add(btnCeas)
+        Controls.Add(prgMinute)
+        Controls.Add(prgSecunde)
+        Controls.Add(prgOre)
+        Controls.Add(btnReset)
+        Controls.Add(btnStart)
+        Controls.Add(txtMinute)
+        Controls.Add(txtSecunde)
+        Controls.Add(txtZecimi)
+        Controls.Add(txtOra)
+        Controls.Add(lblZecimi)
+        Controls.Add(lblSecunde)
+        Controls.Add(lblMinute)
+        Controls.Add(lblOre)
+        Name = "frmCronometru"
+        Text = "Cronometru"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents txtValFTVA As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents txtTVA As TextBox
-    Friend WithEvents btnCalcul As Button
-    Friend WithEvents Label3 As Label
-    Friend WithEvents txtValCuTVA As TextBox
-    Friend WithEvents GroupBox2 As GroupBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents txtDTVA As TextBox
-    Friend WithEvents Label5 As Label
-    Friend WithEvents txtDValCuTVA As TextBox
-    Friend WithEvents txtDValFTVA As TextBox
-    Friend WithEvents Label6 As Label
-    Friend WithEvents btnDeducere As Button
-    Friend WithEvents btnClear As Button
-    Friend WithEvents btnClose As Button
+    Friend WithEvents lblOre As Label
+    Friend WithEvents lblMinute As Label
+    Friend WithEvents lblSecunde As Label
+    Friend WithEvents lblZecimi As Label
+    Friend WithEvents txtOra As TextBox
+    Friend WithEvents txtZecimi As TextBox
+    Friend WithEvents txtSecunde As TextBox
+    Friend WithEvents txtMinute As TextBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents prgOre As ProgressBar
+    Friend WithEvents prgSecunde As ProgressBar
+    Friend WithEvents prgMinute As ProgressBar
+    Friend WithEvents btnCeas As Button
+    Friend WithEvents tmrCronometru As Timer
+    Friend WithEvents tmrCeas As Timer
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,26 +1,68 @@
-﻿Public Class frmCalculDeducereTVA
+﻿Public Class frmCronometru
+
+    Dim iZecimi As Integer = 0
+    Dim iSecunde As Integer = 0
+    Dim iMinute As Integer = 0
+    Dim iOra As Integer = 0
 
 
-    Private Sub btnCalcul_Click(sender As Object, e As EventArgs) Handles btnCalcul.Click
-        txtTVA.Text = Val(txtValFTVA.Text) * 0.24
-        txtValCuTVA.Text = Val(txtValFTVA.Text) + Val(txtTVA.Text)
-    End Sub
-    Private Sub btnDeducere_Click(sender As Object, e As EventArgs) Handles btnDeducere.Click
-        txtDTVA.Text = Val(txtDValCuTVA.Text) * 24 / 124
-        txtDValFTVA.Text = Val(txtDValCuTVA.Text) * 100 / 124
-    End Sub
-    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
-        txtValFTVA.Clear()
-        txtTVA.Clear()
-        txtValCuTVA.Clear()
-        txtDValCuTVA.Clear()
-        txtDTVA.Clear()
-        txtDValFTVA.Clear()
+    Private Sub tmrCeas_Tick(sender As Object, e As EventArgs) Handles tmrCeas.Tick
+        txtOra.Text = TimeOfDay.Hour.ToString("00")
+        txtMinute.Text = TimeOfDay.Minute.ToString("00")
+        txtSecunde.Text = TimeOfDay.Second.ToString("00")
+        txtZecimi.Text = TimeOfDay.Millisecond.ToString("00")
     End Sub
 
-    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
-        End
+    Private Sub tmrCronometru_Tick(sender As Object, e As EventArgs) Handles tmrCronometru.Tick
+        iZecimi += 1
+        If iZecimi = 10 Then
+            iSecunde += 1
+            iZecimi = 0
+            If iSecunde = 60 Then
+                iMinute += 1
+                iSecunde = 0
+                If iMinute = 60 Then
+                    iOra += 1
+                    iMinute = 0
+                End If
+            End If
+        End If
+        txtZecimi.Text = iZecimi.ToString("00")
+        txtSecunde.Text = iSecunde.ToString("00")
+        prgSecunde.Value = iSecunde
+        txtMinute.Text = iMinute.ToString("00")
+        prgMinute.Value = iMinute
+        txtOra.Text = iOra.ToString("00")
+        prgOre.Value = iOra
     End Sub
 
+    Private Sub btnCeas_Click(sender As Object, e As EventArgs) Handles btnCeas.Click
+        tmrCeas.Enabled = True
+        tmrCronometru.Enabled = False
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        tmrCeas.Enabled = False
+        tmrCronometru.Enabled = True
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        tmrCeas.Enabled = False
+        tmrCronometru.Enabled = False
+        txtOra.Text = " "
+        txtMinute.Text = " "
+        txtSecunde.Text = " "
+        txtZecimi.Text = " "
+        prgOre.Value = 0
+        prgMinute.Value = 0
+        prgSecunde.Value = 0
+        iZecimi = 0
+        iSecunde = 0
+        iMinute = 0
+        iOra = 0
+    End Sub
+
+    Private Sub frmCronometru_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
-
